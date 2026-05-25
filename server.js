fa8640cb8a87852479f47104702e4164e0795203
emannaw789-pixel <emannaw789@gmail.com>
2026-05-25 11:56:23 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = 5000;
+
+// Home Route
+app.get("/", (req, res) => {
+    res.send("CakeTime Backend Running 🍰");
+});
+
+// Save Order
+app.post("/place-order", (req, res) => {
+
+    const newOrder = req.body;
+
+    fs.readFile("orders.json", "utf8", (err, data) => {
+
+        let orders = [];
+
+        if (!err && data) {
+            orders = JSON.parse(data);
+        }
+
+        orders.push({
+            id: Date.now(),
+            ...newOrder
+        });
+
+        fs.writeFile(
+            "orders.json",
+            JSON.stringify(orders, null, 2),
+            err => {
+
+                if (err) {
+                    return res.status(500).json({
+                        message: "Error saving order"
+                    });
+                }
+
+                res.json({
+                    message: "Order saved successfully 🎉"
+                });
+            }
+        );
+    });
+});
+
+// Get All Orders
+app.get("/orders", (req, res) => {
+
+    fs.readFile("orders.json", "utf8", (err, data) => {
+
+        if (err) {
+            return res.json([]);
+        }
+
+        res.json(JSON.parse(data));
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
