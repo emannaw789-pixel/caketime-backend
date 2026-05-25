195303ee76f1f75cfdd9378f2686bef9f629fd9e
emannaw789-pixel <emannaw789@gmail.com>
2026-05-25 13:37:10 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 8000;
 // Home Route
 app.get("/", (req, res) => {
     res.send("CakeTime Backend Running 🍰");
